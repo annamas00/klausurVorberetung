6e7ba8092de61c166c5c9ac72ce332dc339450d6
Anna <annamasiuta@gmail.com>
2021-02-12 20:07:41 +0100

    oop 1 done

--- src/oop_1_bank.py
+++ src/oop_1_bank.py
@@ -26,7 +26,48 @@ class Account:
     which data objects this class is designed for.
     You have to remove the pass statement and then write some
     code for the class. """
-    pass
+
+    def __init__(self, num, person):
+        self.balance = 0
+        self.number = num
+        self.holder = person
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if (self.balance - amount) < -1000:
+            print("Operation is not possible: insufficient funds")
+        else:
+            self.balance -= amount
+
+    def print_info(self):
+        print(self.holder + "'s balance is " + str(self.balance) + " dollars.")
+
+    def set_num(self, num):
+        self.number = num
+
+    def set_holder(self, person):
+        self.holder = person
+
+    def apply_interest(self):
+        self.balance = self.balance + (self.balance*0.15)
+
+    def __str__(self):
+        res = "***AccountInfo***\n"
+        res += "AccountID:" + str(self.number) + "\n"
+        res += "Holder:" + self.holder + "\n"
+        res += "Balance:" + str(self.balance) + "\n"
+        return res
+
 
 if __name__ == "__main__":
     print("Welcome to the Python Bank!")
+    franksAcc = Account(1, "Frank")
+    franksAcc.balance = 100
+    franksAcc.print_info()
+    franksAcc.set_holder("Francis")
+    franksAcc.print_info()
+    franksAcc.apply_interest()
+    franksAcc.print_info()
+    print(franksAcc)
